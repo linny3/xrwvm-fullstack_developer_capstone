6245a64cabca580d8dbef5cdc82ac1426590f15a
linny3 <ryan.lindberg3@gmail.com>
2025-04-15 17:20:47 -0400

    trying to fix fetchDealers route

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 # Register your models here.
 
 class CarMakeInline(admin.StackedInline):
@@ -12,6 +13,5 @@ class CarModleInline(admin.StackedInline):
     model = CarModel
     extra = 5
 
-admin.site.register(CarMake)
-admin.site.register(CarModel)
+
 
--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -26,25 +26,25 @@ const Dealers = () => {
     }
   }
 
-  const get_dealers = async ()=>{
-    const res = await fetch(dealer_url, {
-      method: "GET"
-    });
-    const retobj = await res.json();
-    if(retobj.status === 200) {
-      let all_dealers = Array.from(retobj.dealers)
-      let states = [];
-      all_dealers.forEach((dealer)=>{
-        states.push(dealer.state)
-      });
+    const get_dealers = async ()=>{
+        const res = await fetch(dealer_url, {
+        method: "GET"
+        });
+        const retobj = await res.json();
+        if(retobj.status === 200) {
+        let all_dealers = Array.from(retobj.dealers)
+        let states = [];
+        all_dealers.forEach((dealer)=>{
+            states.push(dealer.state)
+        });
 
-      setStates(Array.from(new Set(states)))
-      setDealersList(all_dealers)
+        setStates(Array.from(new Set(states)))
+        setDealersList(all_dealers)
+        }
     }
-  }
-  useEffect(() => {
-    get_dealers();
-  },[]);  
+    useEffect(() => {
+        get_dealers();
+    },[]);  
 
 
 let isLoggedIn = sessionStorage.getItem("username") != null ? true : false;
