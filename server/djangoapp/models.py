7bc7a72bc90a72e32ba1d7e33e7dc318210d309a
linny3 <ryan.lindberg3@gmail.com>
2025-04-17 12:37:14 -0400

    fixed linting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -25,9 +25,9 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=5, choices=CAR_TYPES, default='Sedan')
     year = models.IntegerField(default=2025,
-                             validators=[
-                                    MaxValueValidator(2025),
-                                    MinValueValidator(2015)
+                              validators=[
+                                     MaxValueValidator(2025),
+                                     MinValueValidator(2015)
                                 ])
 
     def __str__(self):
