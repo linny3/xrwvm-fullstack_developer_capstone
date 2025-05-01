ec1792d8bf942cfc645e4cc27dab5cd1d93950d2
linny3 <ryan.lindberg3@gmail.com>
2025-05-01 12:41:21 -0400

    still working on cars database

--- server/carsInventory/app.js
+++ server/carsInventory/app.js
@@ -54,3 +54,61 @@ app.get('/carsbymake/:id/:model', async (req, res) => {
     }
 });
 
+app.get('/carsbymaxmileage/:id/:mileage', async (req, res) => {
+    const mileage = req.params.mileage
+    let miles = {}
+    if(mileage <= 50000) {
+        miles = { $lte: 50000 }
+    } else if(mileage > 50000 || mileage <= 100000) {
+        miles = { $gt: 50000, $lte: 100000 }
+    } else if(mileage > 100000 || mileage <= 150000) {
+        miles = { $gt: 100000, $lte: 150000 } 
+    } else if(mileage > 150000 || mileage <= 200000) {
+        miles = { $gt: 150000, $lte: 200000 } 
+    } else if(mileage > 200000) {
+        miles = { $gt: 200000 }
+    }
+    
+    try {
+        const documents = await Cars.find({dealer_id: req.params.id, mileage: miles});
+        res.json(documents)
+    } catch (e) {
+        res.status(500).json({error: 'Error fetching cars by mileage'})
+    }
+});
+
+app.get('/carsbyprice/:id/:price', async (req, res) => {
+    const price = req.params.price
+    const cost_query = {}
+    if(price <= 20000) {
+        cost_query = { $lte: 20000 }
+    } else if(price > 20000 || price <= 40000) {
+        cost_query = { $gt: 20000, $lte: 40000 }
+    } else if(price > 40000 || price <= 60000) {
+        cost_query = { $gt: 40000, $lte: 60000 } 
+    } else if(price > 60000 || price <= 80000) {
+        cost_query = { $gt: 60000, $lte: 80000 } 
+    } else if(price > 80000) {
+        cost_query = { $gt: 80000 }
+    }
+    
+    try {
+        const documents = await Cars.find({dealer_id: req.params.id, price: cost_query});
+        res.json(documents)
+    } catch (e) {
+        res.status(500).json({error: 'Error fetching cars by mileage'})
+    }
+});
+
+app.get('/carsbymake/:id/:year', async (req, res) => {
+    try {
+        const documents = await Cars.find({dealer_id: req.params.id, year: req.params.year})
+        res.json(documents)
+    } catch (e) {
+        res.status(500).json({error: 'Error fetching cars by year'})
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
