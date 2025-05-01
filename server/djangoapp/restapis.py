486073da8fea4bcbcc1476a3072772d4c880939a
linny3 <ryan.lindberg3@gmail.com>
2025-05-01 16:19:50 -0400

    finished cars database

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,6 +10,9 @@ backend_url = os.getenv(
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
+searchcars_url = os.getenv(
+    'searchcars_url',
+    default="http://localhost:3050/")
 
 
 def get_request(endpoint, **kwargs):
@@ -29,6 +32,20 @@ def get_request(endpoint, **kwargs):
         # If any error occurs
         print(f"Error {e}")
 
+def searchcars_request(endpoint, **kwargs):
+    params = ""
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params+key+"="+value+"&"
+    request_url = searchcars_url+endpoint+"?"+params
+
+    print("GET from {} ".format(request_url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as e:
+        print(f"Error {e}")
 
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
@@ -49,3 +66,7 @@ def post_review(data_dict):
         return response.json()
     except Exception as e:
         print(f"Error: {e}")
+
+
+
+
